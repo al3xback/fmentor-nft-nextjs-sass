6225160cd5f311e1c28e2a97be58054c53cb4806
alex <al3xback@gmail.com>
2024-02-27 17:44:43 +0700

    added first commit

--- README.md
+++ README.md
@@ -1,36 +1,31 @@
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+# Frontend Mentor - NFT preview card component solution
 
-## Getting Started
+This is a solution to the [NFT preview card component challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/nft-preview-card-component-SbdUL_w0U). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
 
-First, run the development server:
+## Overview
 
-```bash
-npm run dev
-# or
-yarn dev
-# or
-pnpm dev
-# or
-bun dev
-```
+### The challenge
 
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+Users should be able to:
 
-You can start editing the page by modifying `app/page.js`. The page auto-updates as you edit the file.
+- View the optimal layout depending on their device's screen size
+- See hover states for interactive elements
 
-This project uses [`next/font`](https://nextjs.org/docs/basic-features/font-optimization) to automatically optimize and load Inter, a custom Google Font.
+### Links
 
-## Learn More
+- Live Site URL: -
 
-To learn more about Next.js, take a look at the following resources:
+## My process
 
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
+### Built with
 
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
+- Semantic HTML5 markup
+- Next.js
+- Sass
+- Mobile-first workflow
 
-## Deploy on Vercel
+## Author
 
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+- Frontend Mentor - [@al3xback](https://www.frontendmentor.io/profile/al3xback)
+- Codepen - (https://codepen.io/Al3xback)
+- Twitter - [@al3x_back](https://twitter.com/al3x_back)
--- app/page.js
+++ app/page.js
@@ -1,95 +1,55 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import Image from 'next/image';
+
+import equilibriumImg from '@/assets/images/equilibrium.jpg';
+import avatarImg from '@/assets/images/avatar.png';
+import styles from './page.module.scss';
 
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+	return (
+		<article className={styles.card}>
+			<div className={styles['card-image']}>
+				<Image
+					src={equilibriumImg}
+					width="302"
+					height="302"
+					alt="Equilibrium"
+					priority
+				/>
+			</div>
+			<div className={styles['card-content']}>
+				<h2 className={styles.title}>
+					<a href="#" className="btn btn--link">
+						Equilibrium #3429
+					</a>
+				</h2>
+				<p className={styles.desc}>
+					Our Equilibrium collection promotes balance and calm.
+				</p>
+				<ul className={styles.stats}>
+					<li>
+						<i className="icon-ethereum" aria-hidden="true"></i>
+						<span>0.041 ETH</span>
+					</li>
+					<li>
+						<i className="icon-clock" aria-hidden="true"></i>
+						<span>3 days left</span>
+					</li>
+				</ul>
+				<div className={styles.author}>
+					<Image
+						src={avatarImg}
+						width="33"
+						height="33"
+						alt="Jules Wyvern"
+					/>
+					<span>
+						Creation of{' '}
+						<a href="#" className="btn btn--link">
+							Jules Wyvern
+						</a>
+					</span>
+				</div>
+			</div>
+		</article>
+	);
 }
